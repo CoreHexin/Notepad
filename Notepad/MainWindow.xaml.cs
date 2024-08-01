4444c458bb1bb48a9399d7eda6b548e961f6f841
Hexin <hexin.email@qq.com>
2024-08-01 23:38:08 +0800

    添加另存为功能

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Notepad.ViewModels;
+﻿using Microsoft.Win32;
+using Notepad.ViewModels;
+using System.IO;
+using System.Text;
 using System.Windows;
 
 namespace Notepad
@@ -12,6 +15,24 @@ namespace Notepad
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void saveAsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save data in text file";
+            saveFileDialog.Filter = "Text files|*.txt";
+            saveFileDialog.AddExtension = true;
+
+            if (saveFileDialog.ShowDialog() == true && !string.IsNullOrWhiteSpace(saveFileDialog.FileName))
+            {
+                File.WriteAllText(saveFileDialog.FileName, txtContent.Text, Encoding.UTF8);
+            }
         }
     }
 }
