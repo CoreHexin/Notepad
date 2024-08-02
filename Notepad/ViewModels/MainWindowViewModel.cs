1bcaa2a24102b2dcc892fda2c408bde5278fef58
Hexin <hexin.email@qq.com>
2024-08-02 10:06:02 +0800

    新增缩放功能

--- Notepad/ViewModels/MainWindowViewModel.cs
+++ Notepad/ViewModels/MainWindowViewModel.cs
@@ -8,31 +8,54 @@ namespace Notepad.ViewModels
 {
     class MainWindowViewModel : NotificationObject
     {
-		private string _content;
+        private string _content;
 
-		public string Content
-		{
-			get { return _content; }
-			set 
-			{ 
-				_content = value;
-				RaisePropertyChanged(nameof(Content));
-			}
-		}
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                RaisePropertyChanged(nameof(Content));
+            }
+        }
 
+        private int _contentFontsize;
+
+        public int ContentFontsize
+        {
+            get { return _contentFontsize; }
+            set
+            {
+                _contentFontsize = value;
+                RaisePropertyChanged(nameof(ContentFontsize));
+            }
+        }
+
+        public DelegateCommand ZoomInCommand { get; set; }
+        public DelegateCommand ZoomOutCommand { get; set; }
+        public DelegateCommand ZoomDefaultCommand { get; set; }
         public DelegateCommand NewNoteCommand { get; set; }
 
         public MainWindowViewModel()
         {
-			_content = string.Empty;
+            _content = string.Empty;
+            _contentFontsize = 14;
 
-			NewNoteCommand = new DelegateCommand(NewNoteCommandExecute);
+            NewNoteCommand = new DelegateCommand((param) => { Content = string.Empty; });
+            ZoomInCommand = new DelegateCommand((param) => 
+            { 
+                if (ContentFontsize <= 96)
+                    ContentFontsize += 4; 
+            });
+            ZoomOutCommand = new DelegateCommand((param) => 
+            { 
+                if (ContentFontsize > 4)
+                    ContentFontsize -= 4; 
+            });
+            ZoomDefaultCommand = new DelegateCommand((param) => { ContentFontsize = 14; });
         }
 
-		private void NewNoteCommandExecute(object? parameter)
-		{
-			Content = string.Empty;
-		}
 
     }
 }
